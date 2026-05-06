67bb3c2ad9b9a2f4d7fc1a6395d490c30f04f2d1
LutfiArda <arda_kara03@outlook.com>
2026-05-06 10:31:22 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowMind.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
